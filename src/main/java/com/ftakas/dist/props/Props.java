354cfc64642a17cca8b626d04433228731ef0762
Pafsanias Ftakas <pafsanias@ftakas.com>
2017-06-18 12:41:48 +0300

    Domain checkpoint.

--- src/main/java/com/ftakas/dist/props/Props.java
+++ src/main/java/com/ftakas/dist/props/Props.java
@@ -1,8 +1,10 @@
 package com.ftakas.dist.props;
 
 import com.ftakas.dist.domain.Clazz;
+import com.ftakas.dist.domain.DObject;
 import com.ftakas.dist.domain.PropertyDefn;
 import com.ftakas.dist.domain.PropertyVal;
+import com.ftakas.dist.domain.property.ClazzPropertyVal;
 import com.ftakas.dist.domain.property.StringPropertyVal;
 
 import java.util.ArrayList;
@@ -31,9 +33,11 @@ public class Props {
     public Clazz createNewClazz(String name, Clazz superClazz, PropertyDefn... propertyDefns) {
         Clazz newClazz = new Clazz();
         PropertyDefn namePropertyDefn = propertyDefnService.getPropertyDefn("name");
+        PropertyDefn superClazzPropertyDefn = propertyDefnService.getPropertyDefn("super");
 
         List<PropertyDefn> newClazzPropertyDefns = new ArrayList<>();
         newClazzPropertyDefns.add(namePropertyDefn);
+        newClazzPropertyDefns.add(superClazzPropertyDefn);
         newClazzPropertyDefns.addAll(Arrays.asList(propertyDefns));
 
         StringPropertyVal namePropertyVal = new StringPropertyVal();
@@ -42,12 +46,46 @@ public class Props {
         namePropertyVal.setPropertyDefn(namePropertyDefn);
         namePropertyVal.setStringList(Arrays.asList(new String[]{ name }));
 
+        ClazzPropertyVal superClazzPropertyVal = new ClazzPropertyVal();
+        superClazzPropertyVal.setDirty(false);
+        superClazzPropertyVal.setInConflict(false);
+        superClazzPropertyVal.setPropertyDefn(superClazzPropertyDefn);
+        superClazzPropertyVal.setClazzList(Arrays.asList(new Clazz[]{ superClazz }));
+
         List<PropertyVal> newClazzProperties = new ArrayList<>();
         newClazzProperties.add(namePropertyVal);
+        newClazzProperties.add(superClazzPropertyVal);
 
         newClazz.setProperties(newClazzProperties);
         newClazz.setPropertyDefns(newClazzPropertyDefns);
 
         return newClazz;
+    }
+
+    public DObject createNewDObject(String name, Clazz clazz, PropertyVal... propertyVals) {
+        DObject newDObject = new DObject();
+        PropertyDefn namePropertyDefn = propertyDefnService.getPropertyDefn("name");
+        PropertyDefn superClazzPropertyDefn = propertyDefnService.getPropertyDefn("super");
+
+        StringPropertyVal namePropertyVal = new StringPropertyVal();
+        namePropertyVal.setDirty(false);
+        namePropertyVal.setInConflict(false);
+        namePropertyVal.setPropertyDefn(namePropertyDefn);
+        namePropertyVal.setStringList(Arrays.asList(new String[]{ name }));
+
+        ClazzPropertyVal clazzPropertyVal = new ClazzPropertyVal();
+        clazzPropertyVal.setDirty(false);
+        clazzPropertyVal.setInConflict(false);
+        clazzPropertyVal.setPropertyDefn(superClazzPropertyDefn);
+        clazzPropertyVal.setClazzList(Arrays.asList(new Clazz[]{ clazz }));
+
+        List<PropertyVal> newDObjectProperties = new ArrayList<>();
+        newDObjectProperties.add(namePropertyVal);
+        newDObjectProperties.add(clazzPropertyVal);
+        newDObjectProperties.addAll(Arrays.asList(propertyVals));
+
+        newDObject.setProperties(newDObjectProperties);
+
+        return newDObject;
     }
 }
